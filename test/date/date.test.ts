0bec04563ad888afe7979aebf348aab042fa1d55
FOCKUSTY <wwddwwwd@mail.ru>
2024-10-02 17:44:38 +0500

    tests updated

--- test/date/date.test.ts
+++ test/date/date.test.ts
@@ -7,15 +7,24 @@ const formatter = new DateFormatter();
 
 describe('DateFormatter', () => {
     (() => {
-        const formatted = formatter.Date(formatter.date, 'dd.MM.yyyy').split('.')
+        const formatted = formatter.Date(formatter.date, 'dd.MM.yyyy').split('.');
         const numbers: [number, number, number] = [
             Number(formatted[0]),
             Number(formatted[1]),
             Number(formatted[2])
         ];
 
+        const timeDMY = formatter.toLocaleDMY(formatter.time);
+        const timeNum = formatter.toLocaleDMY(numbers);
+
+        const time: [string, string][] = [];
+
+        for(let i=0; i<3; i++)
+            time.push([`${timeDMY[i]}`, `${timeNum[i]}`]);
+
         const tests: TestsType = [
-            [`${Math.floor(formatter.Timestamp(numbers)/100_000)}`, `${Math.floor(formatter.date.getTime()/100_000_000)}`]
+            [`${Math.floor(formatter.Timestamp(numbers)/100_000)}`, `${Math.floor(formatter.date.getTime()/100_000_000)}`],
+            ...time
         ];
 
         new Test('Timestamp(...)', tests).execute();
