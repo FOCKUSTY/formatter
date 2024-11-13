c65c62a4422d962727a52fc42b031d61fcb196ac
FOCKUSTY <wwddwwwd@mail.ru>
2024-11-13 18:35:26 +0500

    bug fixed

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import Formatter from "./formatter";
 import DateFormatter from "./date/date";
 import Time from "./date/time";
-import { Colors } from "colors";
+import { Colors } from "./colors";
 
 export { DateFormatter, Time, Colors };
 
